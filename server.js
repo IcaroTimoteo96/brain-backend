8e0bc13e1aef3f8df5b824561095241d3195d024
IcaroTimoteo96 <46626881+IcaroTimoteo96@users.noreply.github.com>
2021-01-18 23:15:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
  app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.send(db.users);
+    res.send("its working");
 })
 app.post('/signin' , (req, res) => {
         db.select('email','hash').from('login')
@@ -94,6 +94,6 @@ app.put('/image',(req,res)=>{
     .catch(err=> res.status(400).json('not getting entries'))
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
+/*app.listen(process.env.PORT || 3000, ()=>{
     console.log(`Server is listening on port ${process.env.PORT}`)
-});
+});*/
